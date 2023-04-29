b73ce93304feeae326288025a73f152a607b26ef
Marco Aliaga <54997210+kharne8@users.noreply.github.com>
2023-04-29 17:06:30 -0500

    corrected group_policy.ps1

--- group_policy.ps1
+++ group_policy.ps1
@@ -1,8 +1,8 @@
 # Set Password complexity policy to Enabled
 Set-ItemProperty -Path "HKLM:\SECURITY\Policies\Microsoft\Windows NT\SecEdit\GptTmpl.inf\Account" -Name "PasswordComplexity" -Value 1
 
-# Set RelaxMinimumPasswordLengthLimits to 1
-Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Control\SAM" -Name "RelaxMinimumPasswordLengthLimits" -Value 1
+# Ensure 'MSS: (DisableIPSourceRouting) IP source routing protection level (protects against packet spoofing)' is set to 'Enabled
+Set-ItemProperty -Path "HKLM:\System\CurrentControlSet\Services\Tcpip\Parameters" -Name "DisableIPSourceRouting" -Value 2
 
 # Force restart the computer
 Restart-Computer -Force
